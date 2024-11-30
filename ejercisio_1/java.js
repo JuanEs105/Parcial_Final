abe6ae915beb19c7c30e6b47a9e7c70ae0dd32dd
CIN <seracevedo@uniboyaca.edu.co>
2024-11-30 12:36:54 -0500

    prueba

---
+++ ejercisio_1/java.js
@@ -0,0 +1,12 @@
+
+{
+promedio (nota1, nota2, nota3, nota4)
+    promedio= (((nota1/0,20)+(nota2/0,20)+(nota3/0,20)+(nota4/0,40)/4));
+    if (promedio <= 3) {
+        promedio= "aprobado"; 
+    }
+    else {
+        promedio="reprobado";
+    }
+} 
+
